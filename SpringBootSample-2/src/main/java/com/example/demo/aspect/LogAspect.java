85b633169bfb669308f2fd576db3ab2f78ebbc8a
ShineYamamoto <love.of.240.yen@gmail.com>
2026-09-23 01:15:28 +0900

    13-2

---
+++ SpringBootSample-2/src/main/java/com/example/demo/aspect/LogAspect.java
@@ -0,0 +1,32 @@
+package com.example.demo.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Aspect
+@Component
+@Slf4j
+public class LogAspect {
+
+	/** 対象：[UserService]クラス名に含んでいること */
+	@Pointcut("execution(* *..*UserService*.*(..))")
+	public void userService() {}
+	
+	/** サービスの実行前にログを出力する */
+	@Before("userService()")
+	public void startLog(JoinPoint jp) {
+		log.info("メソッド開始: {}", jp.getSignature());
+	}
+	
+	/** サービスの実行後にログを出力する */
+	@After("userService()")
+	public void endLog(JoinPoint jp) {
+		log.info("メソッド終了: {}", jp.getSignature());
+	}
+}
